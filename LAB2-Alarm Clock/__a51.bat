2b432b780d119dc75658ffacf8aada9ec03a54af
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2023-02-27 11:26:54 -0800

    Added Files

---
+++ LAB2-Alarm Clock/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\kcgro\Documents\GitHub\ELEC291\LAB2-Alarm Clock\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\kcgro\Documents\GitHub\ELEC291\LAB2-Alarm Clock\LAB2.asm"
+echo Crosside_Action Set_Hex_File C:\Users\kcgro\Documents\GitHub\ELEC291\LAB2-Alarm Clock\LAB2.HEX
